b728a00b0304223b52d467c4d2ed20bf13888d0c
Wei-Ting Yap <weitingyp@gmail.com>
2020-09-26 23:14:08 +0100

    Initial model declaration User - for Alfonso to finish

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ class User(db.Model): #create table using db.Model base class to store tasks
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(200))
     desc = db.Column(db.String,default='')
+    #TODO: Alfonso to design database schema 
 
 db.create_all() #create all tables
 db.session.commit()
