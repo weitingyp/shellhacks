764bdfd249c22bc56c314e0070609dbc8d462a5a
ggiande <ggarciadeleon@csustan.edu>
2020-09-27 02:47:58 -0700

    pip freeze

--- frontEnd/react-flask-app/src/TweetBox.js
+++ frontEnd/react-flask-app/src/TweetBox.js
@@ -8,8 +8,9 @@ function TweetBox() {
                 <div className="tweetBox__input">
                     <Avatar src="http://t2.gstatic.com/images?q=tbn:ANd9GcS4QaTYDXxUfYqmm3IohrEKYxl8HkyukFg-QWJCQtZ5f_JqsXvUBSR5dIHIu7reOKCvgyXoozSNceVHXFEMS7g"/>
                     <input placeholder="What's Happpening?" type="text" />
-                    {/* <input placeholder="Enter image url"/> */}
+
                 </div>
+                <input className="tweetBox__inputImage" placeholder="Optional: Enter image URL" type="text"/>
                 <Button className="tweetBox__tweetButton">Tweet</Button>
             </form>    
         </div>
