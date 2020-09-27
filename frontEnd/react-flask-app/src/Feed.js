4c7f176ac53cebf0e1a4ef77a8e1f896f5620a55
ggiande <ggarciadeleon@csustan.edu>
2020-09-27 02:19:45 -0700

    Timelime ant TweetBox

---
+++ frontEnd/react-flask-app/src/Feed.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Feed.css';
+import TweetBox from './TweetBox';
+function Feed() {
+    return (
+        <div className="feed">
+            {/* Header */}
+            <div className="feed__header">
+                <h2>Home</h2>
+            </div>
+
+            {/* TweetBox */}
+            <TweetBox />
+            {/* Timeline */}
+        </div>
+    )
+}
+
+export default Feed
