9d8d597430c767bfbbe28d4c75eb91842684f1c8
ggiande <ggarciadeleon@csustan.edu>
2020-09-27 06:43:36 -0700

    force push

--- frontEnd/react-flask-app/src/TweetBox.js
+++ frontEnd/react-flask-app/src/TweetBox.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import './TweetBox.css';
 import {Avatar, Button} from "@material-ui/core";
+import ImageIcon from '@material-ui/icons/Image';
+import GifIcon from '@material-ui/icons/Gif';
+import PollIcon from '@material-ui/icons/Poll';
+import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
+import InsertInvitationIcon from '@material-ui/icons/InsertInvitation';
 function TweetBox() {
     return (
         <div className="tweetBox">
@@ -10,8 +15,21 @@ function TweetBox() {
                     <input placeholder="What's Happpening?" type="text" />
 
                 </div>
-                <input className="tweetBox__inputImage" placeholder="Optional: Enter image URL" type="text"/>
-                <Button className="tweetBox__tweetButton">Tweet</Button>
+                {/* <input className="tweetBox__inputImage" placeholder="Optional: Enter image URL" type="text"/>
+                Include a row of icons: image, gif, poll, emoji, schedule */}
+
+
+                <div className="tweetBox__footer">
+                    <ImageIcon font-size="small"/>
+                    <GifIcon font-size="small"/>
+                    <PollIcon font-size="small"/>
+                    <EmojiEmotionsIcon font-size="small"/>
+                    <InsertInvitationIcon font-size="small"/>
+                    <Button className="tweetBox__tweetButton">Tweet</Button>
+                </div>
+
+
+
             </form>    
         </div>
     )
